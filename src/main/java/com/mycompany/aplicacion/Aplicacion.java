1642f4e03b2796ace98929ed19e146fe4951521b
Andrea <andreasoleraliaga@gmail.com>
2022-11-22 16:13:19 +0100

    primera version

--- src/main/java/com/mycompany/aplicacion/Aplicacion.java
+++ src/main/java/com/mycompany/aplicacion/Aplicacion.java
@@ -11,7 +11,7 @@ package com.mycompany.aplicacion;
  */
 public class Aplicacion {
          
-private static String veredicto(String valor1, String valor2) {       
+public static String veredicto(String valor1, String valor2) {       
     if (valor1.equals(valor2)) {
         return "Bien. Pero una hora de estudio para cada módulo puede ser insuficiente.";
         }
