996f87a67ddb78d0f0fcd655d66a91e0d1913663
Andrea <andreasoleraliaga@gmail.com>
2022-11-22 16:12:04 +0100

    primera version

---
+++ src/main/java/com/mycompany/aplicacion/Aplicacion.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package com.mycompany.aplicacion;
+
+/**
+ *
+ * @author alia_
+ */
+public class Aplicacion {
+         
+private static String veredicto(String valor1, String valor2) {       
+    if (valor1.equals(valor2)) {
+        return "Bien. Pero una hora de estudio para cada módulo puede ser insuficiente.";
+        }
+    if (Integer.valueOf(valor1) < Integer.valueOf(valor2)){
+        return "Poco tiempo de estudio. Debes dedicar más tiempo.";
+        }
+        return "Ideal. Trabajas los contenidos en casa.";
+    }  
+     public static void main(String[] args) {
+        System.out.println("Nombre alumno " + args[0] + " " + args[1]);
+        System.out.println(veredicto(args[3],args[2]));
+    }
+}
